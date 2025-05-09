7f31e6831e1e88a6bb604efce20c93e76fb188af
DenVik <denyagarkusha2@gmail.com>
2025-05-09 09:10:15 +0200

    nav

---
+++ main.js
@@ -0,0 +1,102 @@
+const mainBtn = document.getElementById("navigation-js");
+const wrapper = document.querySelector(".wrapper");
+let click = 0;
+
+const transformArr1 = [
+  "48px",
+  "34px",
+  "53px",
+  "-4px",
+  "28px",
+  "-34px",
+  "-11px",
+  "-34px",
+  "-37px",
+  "-6px",
+];
+
+const transformArr2 = [
+  "10px",
+  "10px",
+  "10px",
+  "10px",
+  "10px",
+  "10px",
+  "10px",
+  "10px",
+  "10px",
+  "10px",
+];
+
+function animateMenu(arr, btnA1, btnA2) {
+  mainBtn.children[0].style.right = arr[0];
+  mainBtn.children[0].style.top = arr[1];
+  mainBtn.children[1].style.right = arr[2];
+  mainBtn.children[1].style.top = arr[3];
+  mainBtn.children[2].style.right = arr[4];
+  mainBtn.children[2].style.top = arr[5];
+  mainBtn.children[3].style.right = arr[6];
+  mainBtn.children[3].style.top = arr[7];
+  mainBtn.children[4].style.right = arr[8];
+  mainBtn.children[4].style.top = arr[9];
+
+  mainBtn.style.right = btnA1;
+  mainBtn.style.transform = btnA2;
+}
+
+function onHovers(el1, el2, el3, el4, el5) {
+  wrapper.children[0].style.flex = el1;
+  wrapper.children[1].style.flex = el2;
+  wrapper.children[2].style.flex = el3;
+  wrapper.children[3].style.flex = el4;
+  wrapper.children[4].style.flex = el5;
+}
+
+mainBtn.addEventListener("click", (e) => {
+  if (e.currentTarget.nodeName !== "BUTTON") {
+    return;
+  }
+  if (!click) {
+    animateMenu(transformArr1, "40px", "rotate(23deg)");
+    click += 1;
+  } else {
+
+    animateMenu(transformArr2, "20px", "rotate(0deg)");
+    click = 0;
+  }
+});
+
+mainBtn.children[0].addEventListener("click", (e) => {
+  if (e.currentTarget.nodeName !== "SPAN") {
+    return;
+  }
+  onHovers("4", "1", "1", "1", "1");
+});
+
+mainBtn.children[1].addEventListener("click", (e) => {
+  if (e.currentTarget.nodeName !== "SPAN") {
+    return;
+  }
+  onHovers("1", "4", "1", "1", "1");
+});
+
+mainBtn.children[2].addEventListener("click", (e) => {
+  if (e.currentTarget.nodeName !== "SPAN") {
+    return;
+  }
+  onHovers("1", "1", "4", "1", "1");
+});
+
+mainBtn.children[3].addEventListener("click", (e) => {
+  if (e.currentTarget.nodeName !== "SPAN") {
+    return;
+  }
+  onHovers("1", "1", "1", "4", "1");
+});
+
+mainBtn.children[4].addEventListener("click", (e) => {
+  if (e.currentTarget.nodeName !== "SPAN") {
+    return;
+  }
+  onHovers("1", "1", "1", "1", "4");
+});
